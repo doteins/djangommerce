0ded9a672b742dd1ec36b66b66aa0785695a9407
doteins <112283864+doteins@users.noreply.github.com>
2023-07-28 10:53:03 +0300

    Create about and contac page and update urls and views

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
-from django.url import path
+from django.urls import path
 from . import views
 
 urlpatterns = [
   path('', views.index, name="index"),
+  path('contact/', views.contact, name="contact"),
+  path('about/', views.about, name="about"),
 ]
