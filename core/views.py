1f5b157f82abd99faea9b908f45155006870ba24
doteins <112283864+doteins@users.noreply.github.com>
2023-07-29 21:55:23 +0300

    Update index view function to template with data

--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from product.models import Category, Item
+from .forms import SignupForm
 
 def index(request):
-  return render(request, 'index.html', {})
+  items = Item.objects.filter(status=Item.AVAILABLE)[0:6] # This paginates the results to get only 6 items
+  categories = Category.objects.all()
+  ctx = {
+    "categories": categories,
+    "items": items,
+  }
+  return render(request, 'index.html', ctx)
 
 def contact(request):
   return render(request, 'contact.html', {})
