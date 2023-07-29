7a3fbea10d52486a2e01152dcf85bfb1c29572f9
doteins <112283864+doteins@users.noreply.github.com>
2023-07-29 21:56:19 +0300

    Create signup function view

--- core/views.py
+++ core/views.py
@@ -11,6 +11,18 @@ def index(request):
   }
   return render(request, 'index.html', ctx)
 
+def signup(request):
+  if request.method == 'POST':
+    form = SignupForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect("/login/")
+
+  form = SignupForm()
+  ctx = { "form": form }
+
+  return render(request, 'signup.html', ctx)
+
 def contact(request):
   return render(request, 'contact.html', {})
 
