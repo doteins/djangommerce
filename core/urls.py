fa87f46f467a6a393373eb4082d63f0a5a0c4a59
doteins <112283864+doteins@users.noreply.github.com>
2023-07-29 20:35:17 +0300

    Create app name and url for signup view

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,13 @@ from django.conf import settings
 from django.urls import path
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
   path('', views.index, name="index"),
   path('contact/', views.contact, name="contact"),
   path('about/', views.about, name="about"),
+  path('signup/', views.signup, name="signup"),
 ]
 
 # Do NOT use this settings in production environment!
