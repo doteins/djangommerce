95fde2d4c218ba4cae23a13048fa0c9834fef474
doteins <112283864+doteins@users.noreply.github.com>
2023-08-03 18:38:33 +0300

    Add path to login, and add auth form and template

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
 from . import views
+from .forms import LoginForm
 
 app_name = 'core'
 
@@ -10,7 +12,7 @@ urlpatterns = [
   path('contact/', views.contact, name="contact"),
   path('about/', views.about, name="about"),
   path('signup/', views.signup, name="signup"),
-  path('login/', views.login, name="login"),
+  path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name="login")
 ]
 
 # Do NOT use this settings in production environment!
