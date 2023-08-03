a4fecda4bfbe2129a3855fe92355d1a430caf9e3
doteins <112283864+doteins@users.noreply.github.com>
2023-08-03 18:41:19 +0300

    Create view to add new item and add login_required decorator

--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import NewItemForm
 from .models import Item
 
 def detail(request, pk):
@@ -7,3 +9,18 @@ def detail(request, pk):
   ctx = { "item": item, "related_items": related_items }
   
   return render(request, "detail.html", ctx)
+
+@login_required
+def new_item(request):
+  form = NewItemForm()
+  if request.method == "POST":
+    form = NewItemForm(request.POST, request.FILES)
+    if form.is_valid:
+      item = form.save(commit=False)
+      item.created_by = request.user
+      item.save()
+
+      return redirect('item:detail', pk=item.id)
+  
+  ctx = { "form": form, "title": "New item" }
+  return render(request, "new.html", ctx)
