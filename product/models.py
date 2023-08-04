dd95a4c407c133b27efbe8f4455451c7b078e7a3
doteins <112283864+doteins@users.noreply.github.com>
2023-08-04 20:34:42 +0300

    Update options for Item status

--- product/models.py
+++ product/models.py
@@ -13,9 +13,9 @@ class Category(models.Model):
     return self.name
 
 class Item(models.Model):
-  AVAILABLE = "AV"
-  SOLD = "SO"
-  RESERVED = "RE"
+  AVAILABLE = "Available"
+  SOLD = "Sold"
+  RESERVED = "Reserved"
 
   STATUS_CHOICES = [
     (AVAILABLE, "Available"),
@@ -28,7 +28,7 @@ class Item(models.Model):
   description = models.TextField(blank=False, null=False)
   price = models.DecimalField(max_digits=6, decimal_places=2)
   image = models.ImageField(upload_to='items', blank=True, null=True)
-  status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=AVAILABLE)
+  status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=AVAILABLE)
   created_by = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True, null=False)
   updated_at = models.DateTimeField(auto_now=True)
