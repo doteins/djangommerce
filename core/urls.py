86c319b6574f25bb904ac86eb21ce37e1298a70d
doteins <112283864+doteins@users.noreply.github.com>
2023-08-05 12:56:56 +0300

    Update  login template and set conditional to load media

--- core/urls.py
+++ core/urls.py
@@ -9,12 +9,13 @@ app_name = 'core'
 
 urlpatterns = [
   path('', views.index, name="index"),
-  path('user/<str:username/items', views.user_items, name="dashboard"),
+  path('user/items/', views.user_items, name="user_items"),
   path('contact/', views.contact, name="contact"),
   path('about/', views.about, name="about"),
   path('signup/', views.signup, name="signup"),
-  path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name="login")
+  path('login/', auth_views.LoginView.as_view(extra_context={"title": "Login"}, template_name='generic_form.html', authentication_form=LoginForm), name="login"),
 ]
 
-# Do NOT use this settings in production environment!
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+  # Do NOT use this settings in production environment!
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
