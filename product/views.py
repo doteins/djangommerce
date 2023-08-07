5ca7d0fc385351a1e82949987fa49085c560fbd2
doteins <112283864+doteins@users.noreply.github.com>
2023-08-07 15:09:35 +0300

    Update detail view to render and handle item form

--- product/views.py
+++ product/views.py
@@ -4,9 +4,23 @@ from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 def detail(request, pk):
+  user = request.user
   item = get_object_or_404(Item, pk=pk)
   related_items = Item.objects.filter(category=item.category, status=Item.AVAILABLE).exclude(pk=pk)[0:3]
-  ctx = { "item": item, "related_items": related_items }
+  form = EditItemForm(instance=item)
+  if request.method == "POST":
+    form = EditItemForm(request.POST, request.FILES, instance=item)
+    if form.is_valid:
+      form.save()
+
+      return redirect('item:detail', pk=item.id)
+  
+  ctx = { 
+    "item": item, 
+    "related_items": related_items,
+    "form": form, 
+    "title": "Edit item" 
+  }
   
   return render(request, "detail.html", ctx)
 
@@ -23,22 +37,6 @@ def new_item(request):
       return redirect('item:detail', pk=item.id)
   
   ctx = { "form": form, "title": "New item" }
-  return render(request, "generic_form.html", ctx)
-
-@login_required
-def edit_item(request, pk):
-  user = request.user
-  item = get_object_or_404(Item, pk=pk, created_by=user)
-
-  form = EditItemForm(instance=item)
-  if request.method == "POST":
-    form = EditItemForm(request.POST, request.FILES, instance=item)
-    if form.is_valid:
-      form.save()
-
-      return redirect('item:detail', pk=item.id)
-  
-  ctx = { "form": form, "title": "Edit item" }
   return render(request, "generic_form.html", ctx)
 
 @login_required
