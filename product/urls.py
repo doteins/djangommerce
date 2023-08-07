4dc992f9f1024c41408cf137ed79b852781b6b0d
doteins <112283864+doteins@users.noreply.github.com>
2023-08-07 21:54:48 +0300

    fix view name for brose view

--- product/urls.py
+++ product/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'item'
 
 urlpatterns = [
-    #path('', views.items, name='items'),
+    path('', views.browser, name='items'),
     path('new/', views.new_item, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete_item, name='delete'),
-    # path('<int:pk>/edit/', views.edit_item, name='edit'),
 ]
