a71d4a4082a99bd1ad8c13b2b48fe0e4e93215a8
doteins <112283864+doteins@users.noreply.github.com>
2023-08-08 15:14:09 +0300

    Update contact view and contact template

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.core.mail import send_mail, BadHeaderError
 from product.models import Category, Item
-from .forms import SignupForm
+from .forms import SignupForm, ContactForm
 
 def index(request):
   items = Item.objects.filter(status=Item.AVAILABLE)[0:6] # This paginates the results to get only 6 items
@@ -20,6 +22,7 @@ def signup(request):
   if request.method == 'POST':
     form = SignupForm(request.POST)
     if form.is_valid():
+      form.cleaned_data()
       form.save()
       return redirect("/login/")
     
@@ -35,7 +38,33 @@ def user_items(request):
   return render(request, "user_items.html", ctx)
 
 def contact(request):
-  return render(request, 'contact.html', {})
+  form = ContactForm()
+  if request.method == "POST":
+    form = ContactForm(request.POST)
+    if form.is_valid():
+      name = form.cleaned_data["name"]
+      from_email = form.cleaned_data["from_email"]
+      subject = form.cleaned_data["subject"]
+      message = form.cleaned_data["message"]
+      try:
+        send_mail(
+          from_email,
+          subject,
+          message,
+          ["admin@example.com"], # Recipient email
+          fail_silently=False,
+        )
+      except BadHeaderError:
+        return HttpResponse("Invalid header found")
+
+      ctx = { 
+        "username": name, 
+        "user_email": from_email, 
+        "title": "Thank you" 
+        }
+      return render(request, "contact.html", ctx)
+  ctx = { "form": form, "title": "Contact Us"}
+  return render(request, 'generic_form.html', ctx)
 
 def about(request):
   return render(request, 'about.html', {})
