9e4d31acade13f94baa8b24fa735d22b1face60e
doteins <112283864+doteins@users.noreply.github.com>
2023-08-08 11:17:02 +0300

    Change static folder and add static imgs

--- config/settings.py
+++ config/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'core/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    "core/static",
+    "static",
 ]
 
 # Media uploaded by users goes to this directory
