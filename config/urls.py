04b55e0760b19a801426929cdab3626ba463ff9b
doteins <112283864+doteins@users.noreply.github.com>
2023-08-19 13:47:02 +0300

    Add chat urls

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
     path('items/', include("product.urls")),
+    path('inbox/', include("chat.urls")),
 ]
