0b80ae8377e6116d975b3eb3de6a8df2b8dd971c
Yerold S <112283864+yeroldsan@users.noreply.github.com>
2024-01-10 13:35:06 +0200

    test: Add new test for Category model

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth.models import User
+from .models import Category, Item
+
+class CategoryModelTest(TestCase):
+  def setUp(self):
+    '''
+    Test the creation of a new category
+    '''
+    Category.objects.create(name="Test Category")
+
+  def test_category_str(self):
+    category = Category.objects.get(name="Test Category")
+    self.assertEqual(str(category), "Test Category")
